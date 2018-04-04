deb03fe7f4bab6cb2dac969ce4ba4b3306a50a4f
Derek Stavis <dekestavis@gmail.com>
2018-04-04 11:03:49 -0300

    i18n: add pt-BR as default moment locale

--- packages/pilot/src/initializeI18n.js
+++ packages/pilot/src/initializeI18n.js
@@ -3,6 +3,11 @@ import Backend from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { reactI18nextModule } from 'react-i18next'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+moment.locale('pt-br')
+
 const basePath = process.env.PUBLIC_URL || ''
 
 i18n
