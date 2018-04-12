660930ff67cc78139b1903573681756e9c0cf787
rsmelo <rodrigosm87@gmail.com>
2018-04-12 13:42:39 -0300

    cockpit: add proxy fallback

---
+++ packages/cockpit/src/proxy.js
@@ -0,0 +1,46 @@
+import {
+  complement,
+  either,
+  has,
+  is,
+} from 'ramda'
+
+const isNotObject = either(
+  complement(is(Object)),
+  either(
+    is(Function),
+    is(Array)
+  )
+)
+
+const getProxyHandler = fallback => ({
+  get (target, key) {
+    const hasKey = has(key)
+
+    if (hasKey(target)) {
+      const propInTarget = target[key]
+
+      if (isNotObject(propInTarget)) {
+        return propInTarget
+      } else if (hasKey(fallback)) {
+        return new Proxy(propInTarget, getProxyHandler(fallback[key]))
+      }
+    }
+    if (hasKey(fallback)) {
+      const propInFallback = fallback[key]
+      if (isNotObject(propInFallback)) {
+        return propInFallback
+      }
+      return new Proxy(propInFallback, getProxyHandler(propInFallback))
+    }
+    return undefined
+  },
+})
+
+const proxy = cockpit => (client) => {
+  const cockpitWithClient = cockpit(client)
+
+  return new Proxy(cockpitWithClient, getProxyHandler(client))
+}
+
+export default proxy
