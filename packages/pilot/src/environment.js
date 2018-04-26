ab8a3d73ee6f78a683d30ceb0a594c315543a66a
Derek Stavis <dekestavis@gmail.com>
2018-04-26 11:09:57 -0300

    environment: remove NODE_ENV from env decision

--- packages/pilot/src/environment.js
+++ packages/pilot/src/environment.js
@@ -1,8 +1,5 @@
-const { NODE_ENV, REACT_APP_API_ENVIRONMENT } = process.env
+const { REACT_APP_API_ENVIRONMENT } = process.env
 
-const environment =
-  (NODE_ENV === 'production' || REACT_APP_API_ENVIRONMENT === 'live')
-    ? 'live'
-    : 'test'
+const env = REACT_APP_API_ENVIRONMENT === 'live' ? 'live' : 'test'
 
-export default environment
+export default env
