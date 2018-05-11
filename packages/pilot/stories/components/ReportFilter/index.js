d896732a7e3db4dae5d1e7e20c59298f76f86424
gmbarroso <barroso.guilherme@gmail.com>
2018-05-11 18:27:55 +0000

    report-filter: add component

--- packages/pilot/stories/components/ReportFilter/index.js
+++ packages/pilot/stories/components/ReportFilter/index.js
@@ -5,9 +5,10 @@ import {
   CardContent,
   CardActions,
   DateInput,
-  Input,
   Dropdown,
+  Input,
 } from 'former-kit'
+import Calendar32 from 'emblematic-icons/svg/Calendar32.svg'
 import Search32 from 'emblematic-icons/svg/Search32.svg'
 import style from './style.css'
 
@@ -15,46 +16,60 @@ const findByLabel = 'Filtre pelo nome do relatório'
 
 const options = [
   {
+    value: '00',
+    name: 'Todos os status',
+  },
+  {
     value: '01',
-    name: 'teste',
+    name: 'teste1',
   },
   {
     value: '02',
-    name: 'teste',
+    name: 'teste2',
   },
   {
     value: '03',
-    name: 'teste',
+    name: 'teste3',
   }
 ]
 
-const statusLabel = 'Todos os status'
-
-const fillButton = [
-  'outline',
-  'gradient',
-]
-
-function limparFiltro(){
-  for (i=0; i < length; i++){
-    options.options[i]=null
-  }
-}
+const statusLabel = 'Selecione um status'
 
 const ReportFilter = () => (
   <Card className={style.reportFilter}>
     <form action="/" method="post">
       <CardContent className={style.filterContent}>
-        <Input className={style.searchField} icon={<Search32 width={16} height={16} />} placeholder={findByLabel} />
-        <Dropdown options={options} placeholder={statusLabel} />
-        <CardActions>
-        <Button fill={fillButton[0]} onClick={limparFiltro}>
+        <div className={style.inputs}>
+          <DateInput
+            className={style.dateField}
+            icon={<Calendar32 width={16} height={16} />}
+          />
+          <Input
+            className={style.searchField}
+            icon={<Search32 width={16}
+              height={16} />}
+            placeholder={findByLabel}
+          />
+          <Dropdown
+            className={style.statusField}
+            options={options}
+            value="teste2"
+          />
+        </div>
+        <div className={style.buttons}>
+          <Button
+            fill='outline'
+            onClick=""
+          >
             Limpar Filtros
-        </Button>
-          <Button fill={fillButton[1]} onClick=''>
+          </Button>
+          <Button
+            fill='gradient'
+            onClick=""
+          >
             Filtrar
-        </Button>
-        </CardActions>
+          </Button>
+        </div>
       </CardContent>
     </form>
   </Card>
