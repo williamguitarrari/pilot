1b43cd3d4844e7b996dcd7b4dc45499336975173
gmbarroso <barroso.guilherme@gmail.com>
2018-05-16 18:41:00 +0000

    report-list: add collapse control

--- packages/pilot/stories/containers/ReportList/index.js
+++ packages/pilot/stories/containers/ReportList/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {
+  Button,
   Card,
   CardTitle,
   CardContent,
   CardSection,
   CardSectionDoubleLineTitle,
+  Legend,
 } from 'former-kit'
+import reportStatus from './reportStatus.js'
+import reportStatusLegend from './reportStatusLegend.js'
+import TrashIcon from 'emblematic-icons/svg/Trash32.svg'
+import DownloadIcon from 'emblematic-icons/svg/Download32.svg'
 
 const reports = [
   {
@@ -14,9 +20,9 @@ const reports = [
     "status": "waiting",
     "type": "plans",
     "data": {
-        "company_id": "58ebcf091173c91400a0b05d",
-        "created_at": "2018-03-11T00:00:00.000Z",
-        "updated_at": "2018-03-11T00:00:00.000Z"
+      "company_id": "58ebcf091173c91400a0b05d",
+      "created_at": "2018-03-11T00:00:00.000Z",
+      "updated_at": "2018-03-11T00:00:00.000Z"
     },
     "url": null,
     "created_at": "2018-05-16T17:38:49.544Z",
@@ -28,9 +34,9 @@ const reports = [
     "status": "waiting",
     "type": "plans",
     "data": {
-        "company_id": "58ebcf091173c91400a0b05d",
-        "created_at": "2018-03-11T00:00:00.000Z",
-        "updated_at": "2018-03-11T00:00:00.000Z"
+      "company_id": "58ebcf091173c91400a0b05d",
+      "created_at": "2018-03-11T00:00:00.000Z",
+      "updated_at": "2018-03-11T00:00:00.000Z"
     },
     "url": null,
     "created_at": "2018-05-16T17:38:49.544Z",
@@ -42,9 +48,9 @@ const reports = [
     "status": "waiting",
     "type": "plans",
     "data": {
-        "company_id": "58ebcf091173c91400a0b05d",
-        "created_at": "2018-03-11T00:00:00.000Z",
-        "updated_at": "2018-03-11T00:00:00.000Z"
+      "company_id": "58ebcf091173c91400a0b05d",
+      "created_at": "2018-03-11T00:00:00.000Z",
+      "updated_at": "2018-03-11T00:00:00.000Z"
     },
     "url": null,
     "created_at": "2018-05-16T17:38:49.544Z",
@@ -56,48 +62,110 @@ const reports = [
     "status": "ready",
     "type": "plans",
     "data": {
-        "company_id": "58ebcf091173c91400a0b05d",
-        "created_at": "2018-03-11T00:00:00.000Z",
-        "updated_at": "2018-03-11T00:00:00.000Z"
+      "company_id": "58ebcf091173c91400a0b05d",
+      "created_at": "2018-03-11T00:00:00.000Z",
+      "updated_at": "2018-03-11T00:00:00.000Z"
     },
     "url": "http://s3.amazonaws.com/tractor-bucket/public/plans_cjg43qugq000j0wluzdr2rtxj.json?AWSAccessKeyId=AKIAJ5I56BZT2LHB75KQ&Expires=1525278318&Signature=pNDsGG8gEcYawzMjNPrbXkq5DbU%3D",
     "created_at": "2018-04-17T19:45:36.082Z",
     "updated_at": "2018-04-17T20:09:51.418Z"
- },
- {
+  },
+  {
     "object": "report",
     "id": "rep_cjh9eevpv00031uo7st5nscqc",
     "status": "ready",
     "type": "plans",
     "data": {
-        "company_id": "58ebcf091173c91400a0b05d",
-        "created_at": "2018-03-11T00:00:00.000Z",
-        "updated_at": "2018-03-11T00:00:00.000Z"
+      "company_id": "58ebcf091173c91400a0b05d",
+      "created_at": "2018-03-11T00:00:00.000Z",
+      "updated_at": "2018-03-11T00:00:00.000Z"
     },
     "url": "http://s3.amazonaws.com/tract-live/public/plans_cjh9eew06000012rxte25jpkr.json?AWSAccessKeyId=AKIAIUD2DGLYBPF7X3FA&Expires=1526493233&Signature=ce4THQeQV%2Ft%2BDPUnbTHSYo51mCI%3D",
     "created_at": "2018-05-16T17:47:02.035Z",
     "updated_at": "2018-05-16T17:47:02.596Z"
- }
+  }
 ]
 
-const ReportListState = () => (
-  <div>
-    <Card>
-      <CardTitle
-        title="Relatórios - Total de 75"
-      />
-      <CardContent>
-        <CardSection>
-          <CardSectionDoubleLineTitle
-            title="Carta"
-            subtitle="Verifique ou edite as informações da sua empresa"
-            collapsed="false"
-          />
-        </CardSection>
-      </CardContent>
-    </Card>
-  </div>
-)
+// changeStatus = () => {
+//   if (reportStatus.items[0].value = 'waiting') {
+//     console.log('to esperando')
+//     return <span> teste </span>
 
-export default ReportListState
+//   } else if (reportStatus.items[1].value = 'processing') {
+//     console.log('to processando')
+
+//   } else if (reportStatus.items[2].value = 'ready') {
+//     console.log('to pronto')
+
+//   } else if (reportStatus.items[3].value = 'canceled') {
+//     console.log('fui cancelado :(')
+
+//   }
+// }
+
+console.log(reportStatus.items[0].label)
+
+export default class ReportListState extends React.Component {
+  constructor() {
+    super()
+    this.state = { collapsed: false }
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardTitle
+          title="Relatórios - Total de 75"
+        />
+        <CardContent>
+          <CardSection >
+            <CardSectionDoubleLineTitle
+              title="Carta"
+              subtitle="Verifique ou edite as informações da sua empresa"
+              collapsed={this.state.collapsed}
+              icon={<Legend color="#4ca9d7" acronym="ZK"></Legend>}
+              onClick={
+                collapsed => this.setState({ collapsed: !collapsed })
+              }
+              children={<Button fill='outline' icon={<TrashIcon width={12} height={12} />} />}              
+            />
+
+            {!this.state.collapsed &&
+              <CardContent>
+                asdfgbasdigasdgasdgb
+                </CardContent>
+            }
+          </CardSection>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+// const ReportListState = () => (
+//   <div>
+//     <Card>
+//       <CardTitle
+//         title="Relatórios - Total de 75"
+//       />
+//       <CardContent>
+//         <CardSection >
+//           <CardSectionDoubleLineTitle
+//             title="Carta"
+//             subtitle="Verifique ou edite as informações da sua empresa"
+//             collapsed={false}          
+//             icon={<Legend color="#4ca9d7" acronym="ZK"></Legend>}
+
+//           >
+//           <CardContent children="asfgerghhethrtrtjrjrtjrjwj">
+//           </CardContent>
+
+//           </CardSectionDoubleLineTitle>          
+//         </CardSection>
+//       </CardContent>
+//     </Card>
+//   </div>
+// )
+
+// export default ReportListState
 
