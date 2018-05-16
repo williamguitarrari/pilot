9072bcf2307b88101dbddf098da0546e06808266
Deivis Wingert <deiviswingert@hotmail.com>
2018-05-16 14:45:46 -0300

    chart: refactor legend

--- packages/pilot/src/containers/TransactionsList/Charts.js
+++ packages/pilot/src/containers/TransactionsList/Charts.js
@@ -1,98 +1,109 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import {
   Bar,
   BarChart,
   CartesianGrid,
-  Legend,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
 } from 'recharts'
-import { Legend as FormerLegend } from 'former-kit'
+import {
+  Col,
+  Grid,
+  Legend,
+  Row,
+} from 'former-kit'
 import formatCurrency from '../../formatters/currency'
 import formatDate from '../../formatters/longDate'
 import statusLegends from '../../models/statusLegends'
 import style from './style.css'
 
-const renderLegend = ({
-  dataKey, // eslint-disable-line
-  inactive, // eslint-disable-line
-}) => {
-  const status = statusLegends[dataKey]
+const renderLegend = (legendKey) => {
+  const status = statusLegends[legendKey]
 
   return (
-    <span
-      key={dataKey}
-      className={style.legend}
+    <Col
+      desk={2}
+      key={legendKey}
+      palm={2}
+      tablet={2}
+      tv={1}
     >
-      <FormerLegend
+      <Legend
         acronym={status.acronym}
         color={status.color}
-        outline={inactive}
       >
         {status.text}
-      </FormerLegend>
-    </span>
+      </Legend>
+    </Col>
   )
 }
 
-const Charts = ({ data }) => (
-  <ResponsiveContainer width="100%" height={500}>
-    <BarChart
-      data={data}
-      height={300}
-      maxBarSize={17}
-      width={600}
-    >
-      <XAxis
-        axisLine={false}
-        dataKey="name"
-        tick={{ fontSize: 11 }}
-        tickFormatter={formatDate}
-        tickMargin={10}
-      />
-      <YAxis
-        axisLine={false}
-        height={50}
-        tick={{ fontSize: 11 }}
-        tickFormatter={formatCurrency}
-      />
-      <CartesianGrid
-        stroke="#d7d7d7"
-        vertical={false}
-      />
-      <Tooltip
-        formatter={formatCurrency}
-        labelFormatter={formatDate}
-      />
-      <Legend
-        content={({ payload }) => (
-          <span className={style.legendContainer}>
-            { payload.map(renderLegend) }
-          </span>
-        )}
-      />
-      {
-        Object.keys(statusLegends).map(legend => (
-          <Bar
-            dataKey={legend}
-            fill={statusLegends[legend].color}
-            key={legend}
-            name={statusLegends[legend].text}
-            stackId="a"
-            stroke={statusLegends[legend].color}
-            type="monotone"
-          />
-        ))
-      }
-    </BarChart>
-  </ResponsiveContainer>
+const Charts = ({ data, legendsTitle }) => (
+  <Fragment>
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart
+        data={data}
+        height={300}
+        maxBarSize={17}
+        width={600}
+      >
+        <XAxis
+          axisLine={false}
+          dataKey="name"
+          tick={{ fontSize: 11 }}
+          tickFormatter={formatDate}
+          tickMargin={10}
+        />
+        <YAxis
+          axisLine={false}
+          height={50}
+          tick={{ fontSize: 11 }}
+          tickFormatter={formatCurrency}
+        />
+        <CartesianGrid
+          stroke="#d7d7d7"
+          vertical={false}
+        />
+        <Tooltip
+          formatter={formatCurrency}
+          labelFormatter={formatDate}
+        />
+        {
+          Object.keys(statusLegends).map(legend => (
+            <Bar
+              dataKey={legend}
+              fill={statusLegends[legend].color}
+              key={legend}
+              name={statusLegends[legend].text}
+              stackId="a"
+              stroke={statusLegends[legend].color}
+              type="monotone"
+            />
+          ))
+        }
+      </BarChart>
+    </ResponsiveContainer>
+    <div className={style.legendContainer}>
+      <span className={style.legendTitle}> {legendsTitle} </span>
+      <Grid className={style.stretch}>
+        <Row stretch>
+          {Object.keys(statusLegends).map(renderLegend)}
+        </Row>
+      </Grid>
+    </div>
+  </Fragment>
 )
 
 Charts.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object), // eslint-disable-line
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  legendsTitle: PropTypes.string,
+}
+
+Charts.defaultProps = {
+  legendsTitle: '',
 }
 
 export default Charts
