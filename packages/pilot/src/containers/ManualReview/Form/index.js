b6baca214fcfe5043a1de45c410f46931caeaaa4
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-05-20 18:32:37 -0300

    manual-review: add modal and page

---
+++ packages/pilot/src/containers/ManualReview/Form/index.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {
+  Button,
+  Col,
+  FormInput,
+  Grid,
+  ModalActions,
+  ModalContent,
+  Row,
+} from 'former-kit'
+import style from './style.css'
+
+class ManualReviewForm extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      password: '',
+    }
+
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handlePasswordChange (event) {
+    this.setState({
+      password: event.target.value,
+    })
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.onConfirm(this.state.password)
+  }
+
+  render () {
+    const {
+      action,
+      errorMessage,
+      t,
+      transactionId,
+    } = this.props
+    const { password } = this.state
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ModalContent>
+          <Grid>
+            <Row>
+              <Col palm={12} tablet={12} desk={12} tv={12}>
+                <span className={style.instructionMessage}>
+                  {t('pages.manual_review.question_part_1')}
+                  <strong className={style.textHighlight}>
+                    {action === 'approve'
+                      ? t('pages.manual_review.question_action_approve')
+                      : t('pages.manual_review.question_action_refuse')
+                    }
+                  </strong>&nbsp;
+                  {t('pages.manual_review.question_part_2')}
+                  &nbsp;ID#{transactionId},&nbsp;
+                  <strong className={style.textHighlight}>
+                    {t('models.antifraud_analyses.status.refused')}
+                  </strong>&nbsp;
+                  {t('pages.manual_review.question_part_3')}
+                </span>
+              </Col>
+
+              <Col palm={12} tablet={12} desk={12} tv={12} align="center">
+                <FormInput
+                  error={errorMessage}
+                  label={t('pages.manual_review.password_password_label')}
+                  onChange={this.handlePasswordChange}
+                  type="password"
+                  value={password}
+                />
+              </Col>
+
+              <Col palm={12} tablet={12} desk={12} tv={12}>
+                <p className={style.legalWarning}>
+                  {t('pages.manual_review.legal_warning_approves_part_1')}&nbsp;
+                  <strong className={style.textHighlight}>
+                    {t('pages.manual_review.legal_warning_approves_part_2')}
+                  </strong>
+                  {t('pages.manual_review.legal_warning_approves_part_3')}&nbsp;
+                  {action === 'approve'
+                    ? t('pages.manual_review.legal_warning_approves_approve')
+                    : t('pages.manual_review.legal_warning_approves_refuse')
+                  }
+                </p>
+              </Col>
+            </Row>
+          </Grid>
+        </ModalContent>
+        <ModalActions>
+          <Button
+            fill="gradient"
+            type="submit"
+            disabled={!password}
+          >
+            {action === 'approve'
+              ? t('pages.manual_review.approve_action')
+              : t('pages.manual_review.refuse_action')
+            }
+          </Button>
+        </ModalActions>
+      </form>
+    )
+  }
+}
+
+ManualReviewForm.propTypes = {
+  action: PropTypes.oneOf(['approve', 'refuse']).isRequired,
+  errorMessage: PropTypes.string,
+  onConfirm: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+  transactionId: PropTypes.number.isRequired,
+}
+
+ManualReviewForm.defaultProps = {
+  errorMessage: '',
+}
+
+export default ManualReviewForm
---
+++ packages/pilot/stories/containers/ManualReview/index.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import { identity } from 'ramda'
+import PropTypes from 'prop-types'
+import { action as storybookAction } from '@storybook/addon-actions'
+import ManualReview from '../../../src/containers/ManualReview'
+
+const ManualReviewState = ({
+  action,
+  statusMessage,
+  hasError,
+  stepStatus,
+}) => (
+  <ManualReview
+    action={action}
+    hasError={hasError}
+    isOpen
+    loading={false}
+    onCancel={storybookAction('cancel')}
+    onConfirm={storybookAction('submit')}
+    onReprocessRestart={storybookAction('reprocess restart')}
+    onRetry={storybookAction('retry')}
+    onViewTransaction={storybookAction('view transaction')}
+    statusMessage={statusMessage}
+    stepStatus={stepStatus}
+    t={identity}
+    transaction={{
+      amount: 2000000,
+      card: {
+        first_digits: '123456',
+        holder_name: 'Lorem Ipsum de Consectetuer e Amet',
+        last_digits: '7890',
+      },
+      id: 1,
+      payment: {
+        installments: 48,
+      },
+    }}
+    transactionId={12345}
+  />
+)
+
+ManualReviewState.propTypes = {
+  action: PropTypes.oneOf(['approve', 'refuse']).isRequired,
+  hasError: PropTypes.bool,
+  statusMessage: PropTypes.string,
+  stepStatus: PropTypes.shape({
+    confirmation: PropTypes.string,
+    result: PropTypes.string,
+  }),
+}
+
+ManualReviewState.defaultProps = {
+  hasError: false,
+  statusMessage: '',
+  stepStatus: {
+    confirmation: 'current',
+    result: 'pending',
+  },
+}
+
+const ManualReviewStepApproveConfirmation = () => (
+  <ManualReviewState
+    action="approve"
+    stepStatus={{
+      confirmation: 'current',
+      result: 'pending',
+    }}
+  />
+)
+
+const ManualReviewStepApproveResult = () => (
+  <ManualReviewState
+    action="approve"
+    stepStatus={{
+      confirmation: 'success',
+      result: 'current',
+    }}
+  />
+)
+
+const ManualReviewStepRefuseConfirmation = () => (
+  <ManualReviewState
+    action="refuse"
+    stepStatus={{
+      confirmation: 'current',
+      result: 'pending',
+    }}
+  />
+)
+
+const ManualReviewStepRefuseResult = () => (
+  <ManualReviewState
+    action="refuse"
+    stepStatus={{
+      confirmation: 'success',
+      result: 'current',
+    }}
+  />
+)
+
+const ManualReviewStepResultError = () => (
+  <ManualReviewState
+    action="approve"
+    stepStatus={{
+      confirmation: 'success',
+      result: 'error',
+    }}
+  />
+)
+
+export {
+  ManualReviewStepApproveConfirmation,
+  ManualReviewStepApproveResult,
+  ManualReviewStepRefuseConfirmation,
+  ManualReviewStepRefuseResult,
+  ManualReviewStepResultError,
+}
