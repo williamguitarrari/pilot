2693eebbca7c2e8b5e00f3539b1b8b715725c362
rsmelo <rodrigosm87@gmail.com>
2018-05-29 14:40:22 -0300

    transaction-details: add capabilities
    add reprocessable and renfundable
    
    Co-authored-by: leonero <leonardofelipenerone@gmail.com>

---
+++ packages/cockpit/src/transactions/details/isReprocessable.js
@@ -0,0 +1,33 @@
+import moment from 'moment'
+
+const canReprocess = (transaction, data) => {
+  if (transaction.status !== 'refused') {
+    return false
+  }
+
+  if (transaction.subscription_id) {
+    return false
+  }
+
+  if (transaction.payment_method !== 'credit_card') {
+    return false
+  }
+
+  if (moment(transaction.date_created).isBefore(moment().subtract(24, 'hours'))) {
+    return false
+  }
+
+  if (typeof transaction.metadata.pagarme_original_transaction_id !== 'undefined') {
+    return false
+  }
+
+  return data.length === 0
+}
+
+const isReprocessable = (transaction, data) => {
+  const canReprocessTransaction = canReprocess(transaction, data)
+
+  return canReprocessTransaction && transaction.status !== 'processing'
+}
+
+export default isReprocessable
