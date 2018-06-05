663265112a94e3fca7ffeb4a1bccb2d56424a181
gmbarroso <barroso.guilherme@gmail.com>
2018-06-05 18:24:28 +0000

    report-filter: resolving date picker

--- packages/pilot/src/containers/ReportFilter/index.js
+++ packages/pilot/src/containers/ReportFilter/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {
   Button,
   Card,
@@ -7,6 +8,7 @@ import {
   Dropdown,
   Input,
 } from 'former-kit'
+import moment from 'moment'
 import Calendar32 from 'emblematic-icons/svg/Calendar32.svg'
 import Search32 from 'emblematic-icons/svg/Search32.svg'
 import style from './style.css'
@@ -34,48 +36,142 @@ const options = [
 
 // const statusLabel = 'Selecione um status'
 
-const ReportFilter = () => (
-  <Card className={style.reportFilter}>
-    <form action="/" method="post">
-      <CardContent className={style.filterContent}>
-        <div className={style.inputs}>
-          <DateInput
-            className={style.dateField}
-            icon={<Calendar32 width={16} height={16} />}
-          />
-          <Input
-            className={style.searchField}
-            icon={
-              <Search32
-                width={16}
-                height={16}
+// const ReportFilter = () => (
+// Isso é uma função pura.
+// )
+
+// Renderizando componentes
+// Não esquecer do return()
+// renderCard() {
+//   return()
+// }
+
+class ReportFilter extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      // dates: {
+      //   start: props.start,
+      //   end: props.end,
+      // },
+    }
+
+    this.datePresets = [
+      {
+        title: 'Últimos:',
+        items: [
+          {
+            key: 'last-7',
+            title: '7 dias',
+            date: () => -7,
+          },
+          {
+            key: 'last-15',
+            title: '15 dias',
+            date: () => -15,
+          },
+          {
+            key: 'last-30',
+            title: '30 dias',
+            date: () => -30,
+          },
+          {
+            key: 'last-60',
+            title: '60 dias',
+            date: () => -60,
+          },
+        ],
+      },
+    ]
+
+    this.handleDatesChange = this.handleDatesChange.bind(this)
+
+    // Precisamos de um reset?
+    // this.handleReset = this.handleReset.bind(this)
+  }
+
+  componentWillReceiveProps ({ start, end }) {
+    this.setState({ dates: { start, end } })
+  }
+
+  // O reset seria o Cancel?
+  // handleReset () {
+  //   const { start, end } = this.props
+  //   this.setState({ dates: { start, end } })
+  // }
+
+  handleDatesChange (dates) {
+    this.setState({ dates })
+  }
+
+  render () {
+    const { dates } = this.state
+
+    return (
+      <Card className={style.reportFilter}>
+        <form action="/" method="post">
+          <CardContent className={style.filterContent}>
+            <div className={style.inputs}>
+              <DateInput
+                className={style.dateField}
+                presets={this.datePresets}
+                dates={dates}
+                onChange={this.handleDatesChange}
+                active={dates.start && dates.end && true}
+                limits={{
+                lower: moment('01-01-2013', 'DD-MM-YYYY'),
+                upper: moment('01-01-2025', 'DD-MM-YYYY'),
+                }}
+                icon={<Calendar32 width={16} height={16} />}
               />
-            }
-            placeholder={findByLabel}
-          />
-          <Dropdown
-            className={style.statusField}
-            options={options}
-            value="teste2"
-          />
-        </div>
-        <div className={style.buttons}>
-          <Button
-            fill="outline"
-            onClick=""
-          >
-            Limpar Filtros
-          </Button>
-          <Button
-            fill="gradient"
-            onClick=""
-          >
-            Filtrar
-          </Button>
-        </div>
-      </CardContent>
-    </form>
-  </Card>
-)
+              <Input
+                className={style.searchField}
+                icon={
+                  <Search32
+                    width={16}
+                    height={16}
+                  />
+                }
+                placeholder={findByLabel}
+              />
+              <Dropdown
+                className={style.statusField}
+                options={options}
+                value="teste2"
+              />
+            </div>
+            <div className={style.buttons}>
+              <Button
+                fill="outline"
+                onClick=""
+              >
+                Limpar Filtros
+              </Button>
+              <Button
+                fill="gradient"
+                onClick=""
+              >
+                Filtrar
+              </Button>
+            </div>
+          </CardContent>
+        </form>
+      </Card>
+    )
+  }
+}
+
+// ReportFilter.propTypes = {
+//   dates: PropTypes.shape({
+//     start: PropTypes.instanceOf(moment),
+//     end: PropTypes.instanceOf(moment),
+//   })
+// }
+
+// ReportFilter.defaultProps = {
+//     start: '',
+//     end: '',
+// }
 
 export default ReportFilter
