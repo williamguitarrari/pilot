d2b14b580096001c96b56aef27fd75d0ddd87d61
gmbarroso <barroso.guilherme@gmail.com>
2018-06-05 13:30:58 +0000

    report-filter: understanding eslint error

--- packages/pilot/stories/containers/index.js
+++ packages/pilot/stories/containers/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import RecipientListState from './RecipientList'
-import ReportList from './ReportList'
+// import ReportList from './ReportList'
+import ReportFilter from './ReportFilter'
+
 
 storiesOf('Containers', module)
   .add('Recipient list', () => (
