17077bc1c788bc302e66db991bbdbb47b7ed7ff7
deivis <deiviswingert@hotmail.com>
2018-06-15 17:12:17 -0300

    pilot: add reprocess page

---
+++ packages/pilot/src/formatters/apiError.js
@@ -0,0 +1,12 @@
+export const getResponseErrorMessage = ({ message, response }) => {
+  if (response && response.errors) {
+    const error = response.errors[0]
+    return error.message ? error.message : message
+  }
+
+  return message
+}
+
+export default {
+  getResponseErrorMessage,
+}
