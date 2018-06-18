7f4910c78f0747e083890583d1efb990b40e6e68
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-06-18 19:20:45 -0300

    models: add partnersBanksCodes

---
+++ packages/pilot/src/models/partnersBanksCodes.js
@@ -0,0 +1,5 @@
+/**
+ * This banks codes are linked to the transfer cost used for any company recipient.
+ * The withdraw page uses this codes to show the transfer cost in the transferCost field.
+ */
+export default ['237']
