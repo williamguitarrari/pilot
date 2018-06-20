9a31d65693a13bf14b4cbf81a127b6deeb86306a
deivis <deiviswingert@hotmail.com>
2018-06-20 19:41:35 -0300

    currency-input: add stories

--- packages/pilot/stories/components/CurrencyInput/index.js
+++ packages/pilot/stories/components/CurrencyInput/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { FormInput } from 'former-kit'
+import {
+  Col,
+  Grid,
+  FormInput,
+  Row,
+} from 'former-kit'
 import { action } from '@storybook/addon-actions'
 
 import Section from '../../Section'
@@ -7,16 +12,39 @@ import CurrencyInput from '../../../src/components/CurrencyInput'
 
 const CurrencyInputExample = () => (
   <Section>
-    <FormInput
-      label="Currency Input"
-      renderer={props => (
-        <CurrencyInput
-          {...props}
-        />
-      )}
-      onChange={action('handleChange')}
-      value="100"
-    />
+    <Grid>
+      <Row>
+        <Col palm={12} tablet={12} desk={12} tv={12}>
+          <h4> Without max prop </h4>
+          <FormInput
+            label="Currency Input"
+            renderer={props => (
+              <CurrencyInput
+                {...props}
+              />
+            )}
+            onChange={action('handleChange')}
+            value="100"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col palm={12} tablet={12} desk={12} tv={12}>
+          <h4> With max prop 999 </h4>
+          <FormInput
+            label="Currency Input"
+            renderer={props => (
+              <CurrencyInput
+                {...props}
+                max={999}
+              />
+            )}
+            onChange={action('handleChange')}
+            value="100"
+          />
+        </Col>
+      </Row>
+    </Grid>
   </Section>
 )
 
