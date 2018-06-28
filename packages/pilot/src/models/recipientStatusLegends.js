9d2f4c6c5a6965b2bbcfc7bed58738c77a256a58
Ivan Micai da Silva <ivanmicai@gmail.com>
2018-06-28 16:23:54 -0300

    recipients-list: add Recipient Status Legend

---
+++ packages/pilot/src/models/recipientStatusLegends.js
@@ -0,0 +1,28 @@
+const status = {
+  active: {
+    color: '#17c9b2',
+  },
+  affiliation: {
+    color: '#12517c',
+  },
+  blocked: {
+    color: '#fbbb27',
+  },
+  inactive: {
+    color: '#613fc7',
+  },
+  paid: {
+    color: '#17c9b2',
+  },
+  refused: {
+    color: '#da272c',
+  },
+  registration: {
+    color: '#0b6e69',
+  },
+  suspended: {
+    color: '#f16f27',
+  },
+}
+
+export default status
