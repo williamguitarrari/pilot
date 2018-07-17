f7416d1b86dd0d1ecca8761e32403691f897160e
Wallace Oliveira <wallace.oliveira@pagar.me>
2018-07-17 17:51:54 -0300

    configurations: add configuration card form
    Este PR adiciona o componete ConfigurationCardForm, com ele nós
    podemos simplesmente usar o mesmo card alterando apenas algumas
    props, facilitando o desenvolvimento.
    
    resolve #784

---
+++ packages/pilot/src/components/ConfigurationCardForm/index.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Form from 'react-vanilla-form'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardSection,
+  CardSectionDoubleLineTitle,
+} from 'former-kit'
+
+const ConfigurationCardForm = ({
+  children,
+  collapsed,
+  data,
+  icon,
+  onCancel,
+  onClick,
+  onConfirm,
+  subtitle,
+  title,
+  t,
+  validation,
+}) => (
+  <Card>
+    <CardSection>
+      <CardSectionDoubleLineTitle
+        title={title}
+        subtitle={subtitle}
+        collapsed={collapsed}
+        onClick={onClick}
+        icon={icon}
+      />
+      {!collapsed &&
+        <Form
+          data={data}
+          validation={validation}
+          validateOn="blur"
+          onSubmit={onConfirm}
+        >
+          <CardContent>{children}</CardContent>
+
+          <CardActions>
+            <Button
+              fill="outline"
+              onClick={onCancel}
+              type="reset"
+            >
+              {t('configurations.card_form.cancel')}
+            </Button>
+            <Button
+              fill="gradient"
+              type="submit"
+            >
+              {t('configurations.card_form.confirm')}
+            </Button>
+          </CardActions>
+        </Form>
+      }
+    </CardSection>
+  </Card>
+)
+
+ConfigurationCardForm.propTypes = {
+  children: PropTypes.node.isRequired,
+  collapsed: PropTypes.bool,
+  data: PropTypes.shape({}).isRequired,
+  icon: PropTypes.element.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired,
+  validation: PropTypes.shape({}),
+}
+
+ConfigurationCardForm.defaultProps = {
+  collapsed: false,
+  validation: null,
+}
+
+export default ConfigurationCardForm
---
+++ packages/pilot/stories/components/ConfigurationCardForm/index.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { action } from '@storybook/addon-actions'
+import {
+  FormDropdown,
+  Spacing,
+} from 'former-kit'
+import IconBarcode from 'emblematic-icons/svg/BarCode32.svg'
+import ConfigurationCardForm from '../../../src/components/ConfigurationCardForm'
+
+import style from './style.css'
+
+const initialDataState = {
+  installments: '2',
+  instructions: 'What',
+}
+
+const collapsedAction = action('collapsed')
+const cancelAction = action('cancel')
+const confirmAction = action('confirm')
+
+class ConfigurationCardFormExample extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      collapsed: false,
+      data: initialDataState,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleCancel = this.handleCancel.bind(this)
+    this.toggleCollapse = this.toggleCollapse.bind(this)
+  }
+
+  handleSubmit (data, error) {
+    if (!error) {
+      this.setState({ data })
+    }
+
+    confirmAction(this.state.data)
+  }
+
+  handleCancel () {
+    this.setState({
+      data: initialDataState,
+    }, () => cancelAction(this.state.data))
+  }
+
+  toggleCollapse () {
+    const { collapsed } = this.state
+    this.setState({
+      collapsed: !collapsed,
+    }, () => collapsedAction(this.state.collapsed))
+  }
+
+  render () {
+    const {
+      collapsed,
+      data: {
+        installments,
+        instructions,
+      },
+    } = this.state
+
+    const { t } = this.props
+
+    return (
+      <ConfigurationCardForm
+        title={t('configurations.boleto.title')}
+        subtitle={t('configurations.boleto.subtitle')}
+        data={{
+          installments,
+          instructions,
+        }}
+        onCancel={this.handleCancel}
+        onClick={this.toggleCollapse}
+        onConfirm={this.handleSubmit}
+        collapsed={collapsed}
+        icon={<IconBarcode width={16} height={16} />}
+        t={translate => translate}
+      >
+        <p>{t('configurations.boleto.description')}</p>
+
+        <div className={style.content}>
+          <FormDropdown
+            label={t('configurations.boleto.installments_title')}
+            name="installments"
+            options={[
+              { name: '1', value: '1' },
+              { name: '2', value: '2' },
+            ]}
+          />
+          <Spacing size="small" />
+          <FormDropdown
+            label={t('configurations.boleto.instructions_title')}
+            name="instructions"
+            options={[
+              { name: 'What', value: 'What' },
+              { name: 'is', value: 'is' },
+              { name: 'this?', value: 'this?' },
+            ]}
+          />
+        </div>
+      </ConfigurationCardForm>
+    )
+  }
+}
+
+ConfigurationCardFormExample.propTypes = {
+  t: PropTypes.func,
+}
+
+ConfigurationCardFormExample.defaultProps = {
+  t: t => t,
+}
+
+export default ConfigurationCardFormExample
