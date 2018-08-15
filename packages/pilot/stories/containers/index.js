cf4ff23ab6b5583bb0d05959754101df65345f16
gmbarroso <barroso.guilherme@gmail.com>
2018-08-15 17:35:21 -0300

    recipient: add confirm step

--- packages/pilot/stories/containers/index.js
+++ packages/pilot/stories/containers/index.js
@@ -7,6 +7,8 @@ import BankAccountSelector from './Settings/BankAccount/Selector'
 import Balance from './Balance'
 import BankAccountForm from './Settings/BankAccount/Add'
 import AddAccount from './AddRecipient/BankAccountStep/AddAccount'
+import BankAccountStep from './AddRecipient/BankAccountStep'
+import ConfirmRecipientStep from './AddRecipient/ConfirmStep'
 import BoletoForm from './Refund/BoletoForm'
 import BoletoRefundConfirm from './Refund/BoletoConfirmation'
 import BoletoRefundResult from './Refund/BoletoResult'
@@ -95,6 +97,9 @@ storiesOf('Containers|Page containers', module)
   ))
   .add('Add Account', () => (
     <AddAccount />
+  ))
+  .add('Recipient Confirm Step', () => (
+    <ConfirmRecipientStep />
   ))
   .add('Bank Account Step', () => (
     <BankAccountStep />
