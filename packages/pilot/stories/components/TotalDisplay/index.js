ab80aa31cfdfc7b1fce8a6ad5600fd554781dba2
Miguel <oieusouamiguel@gmail.com>
2018-08-17 17:59:41 -0300

    total-display: refactor component font size

--- packages/pilot/stories/components/TotalDisplay/index.js
+++ packages/pilot/stories/components/TotalDisplay/index.js
@@ -16,16 +16,20 @@ const TotalDisplayExample = () => (
       <Grid>
         <Row>
           <Col palm={12} tablet={12} desk={12}>
+            <h2>Total Display without colors</h2>
             <TotalDisplay
               align="center"
-              title="Valor Capturado"
               amount={1000000000}
+              amountSize="large"
               color="#37cc9a"
               subtitle={
                 <span>
                   Capturado em 10/03/2018 às 14:15h
                 </span>
               }
+              title="Valor Capturado"
+              titleColor="#757575"
+              titleSize="small"
             />
           </Col>
         </Row>
@@ -34,8 +38,8 @@ const TotalDisplayExample = () => (
           <Col palm={12} tablet={12} desk={12}>
             <TotalDisplay
               align="start"
-              title="Total de Saídas"
               amount={-500000}
+              amountSize="large"
               color="#ff796f"
               subtitle={
                 <span>
@@ -43,6 +47,9 @@ const TotalDisplayExample = () => (
                   Outras saídas: R$ 0,75 <IconInfo width={12} height={12} />
                 </span>
               }
+              title="Total de Saídas"
+              titleColor="#757575"
+              titleSize="small"
             />
           </Col>
         </Row>
@@ -51,19 +58,83 @@ const TotalDisplayExample = () => (
           <Col palm={12} tablet={12} desk={12}>
             <TotalDisplay
               align="end"
-              title="Valor Líquido"
               amount={9995000000}
+              amountSize="large"
               color="#4ca9d7"
               subtitle={
                 <span>
                   Data a receber: 20/03/2018
                 </span>
               }
+              title="Valor Líquido"
+              titleColor="#757575"
+              titleSize="small"
+            />
+          </Col>
+        </Row>
+      </Grid>
+      <Grid>
+        <Row>
+          <Col palm={12} tablet={12} desk={12}>
+            <h2>Total Display with colors</h2>
+            <TotalDisplay
+              align="center"
+              amount={1000000000}
+              amountSize="huge"
+              color="#37cc9a"
+              subtitle={
+                <span>
+                  Capturado em 10/03/2018 às 14:15h
+                </span>
+              }
+              title="Valor Capturado"
+              titleColor="#37cc9a"
+              titleSize="medium"
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col palm={12} tablet={12} desk={12}>
+            <TotalDisplay
+              align="start"
+              amount={-500000}
+              amountSize="huge"
+              color="#ff796f"
+              subtitle={
+                <span>
+                  MDR: R$ 6,00 | Valor estornado: R$ 15,00 <br />
+                  Outras saídas: R$ 0,75 <IconInfo width={12} height={12} />
+                </span>
+              }
+              title="Total de Saídas"
+              titleColor="#ff796f"
+              titleSize="medium"
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col palm={12} tablet={12} desk={12}>
+            <TotalDisplay
+              align="end"
+              amount={9995000000}
+              amountSize="huge"
+              color="#4ca9d7"
+              subtitle={
+                <span>
+                  Data a receber: 20/03/2018
+                </span>
+              }
+              title="Valor Líquido"
+              titleColor="#4ca9d7"
+              titleSize="medium"
             />
           </Col>
         </Row>
       </Grid>
     </Section>
+
   </Fragment>
 )
 
