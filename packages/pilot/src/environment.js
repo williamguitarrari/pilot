11b5ce8ca71150fecffcdec6e4cfef7fc690e81c
lucasjs <lucas.tcjb@gmail.com>
2018-08-22 21:19:26 -0300

    chore: add environment urls

--- packages/pilot/src/environment.js
+++ packages/pilot/src/environment.js
@@ -1,5 +1,22 @@
-const { REACT_APP_API_ENVIRONMENT } = process.env
+import { defaultTo } from 'ramda'
 
-const env = REACT_APP_API_ENVIRONMENT === 'live' ? 'live' : 'test'
+const {
+  REACT_APP_API_ENVIRONMENT,
+  REACT_APP_LIVE_URL,
+  REACT_APP_TEST_URL,
+} = process.env
+
+const getLiveUrl = defaultTo('https://beta.dashboard.stg.pagarme.net/latest/index.html')
+const getTestUrl = defaultTo('https://beta.dashboard.sandbox.stg.pagarme.net/latest/index.html')
+const getEnv = defaultTo('test')
+
+const env = getEnv(REACT_APP_API_ENVIRONMENT)
+const liveUrl = getLiveUrl(REACT_APP_LIVE_URL)
+const testUrl = getTestUrl(REACT_APP_TEST_URL)
+
+export {
+  liveUrl,
+  testUrl,
+}
 
 export default env
