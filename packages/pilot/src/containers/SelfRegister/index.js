ff3fa3aeb8e3c8ca30b45bda996f00408bf2ac23
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-09-13 14:41:39 -0300

    self-register: add abstraction to waitings page

--- packages/pilot/src/containers/SelfRegister/index.js
+++ packages/pilot/src/containers/SelfRegister/index.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  Button,
-  Card,
-  CardContent,
-  Col,
-  Grid,
-  Row,
-} from 'former-kit'
-import ArrowBack from 'emblematic-icons/svg/ChevronBack24.svg'
-import classNames from 'classnames'
 
-import Logo from '../../pages/logo.svg'
-import SelfRegisterBulletSteps from '../../components/SelfRegister/BulletSteps'
-import SelfRegisterForm from './form'
-import style from './style.css'
+import SelfRegisterForm from './Form'
+import SelfRegisterWaiting from './Waiting'
+
+const isForm = (step) => {
+  const forms = [
+    'create-account',
+    'check-cnpj',
+    'type-cnpj',
+    'without-cnpj',
+    'company-data',
+    'partner-data',
+    'partner-address',
+    'already-sell',
+    'business-detail-present',
+    'business-detail-future',
+    'sales-amount-present',
+    'sales-amount-future',
+    'contract',
+  ]
+
+  return forms.includes(step)
+}
 
 const SelfRegister = ({
   onPreviousButton,
@@ -22,67 +30,27 @@ const SelfRegister = ({
   onSubmit,
   step,
   t,
-}) => (
-  <div className={style.fullScreen}>
-    <Grid className={style.blankCard}>
-      <Row className={style.contentRow}>
-        <Col tv={12} desk={12} tablet={12} palm={12} className={style.columnPadding}>
-          <Card className={style.stretchVertically}>
-            <CardContent className={style.stretchVertically}>
-              <Grid fullHeight>
-                <Row className={style.stretchVertically}>
-                  <Col tv={3} desk={3} tablet={3} palm={3}>
-                    <div className={style.header}>
-                      <Button
-                        fill="outline"
-                        icon={<ArrowBack height={12} width={12} />}
-                        onClick={onPreviousButton}
-                        size="tiny"
-                        type="submit"
-                      >
-                        {t('pages.self_register.return')}
-                      </Button>
-                    </div>
-                  </Col>
+}) => {
+  if (isForm(step)) {
+    return (
+      <SelfRegisterForm
+        onPreviousButton={onPreviousButton}
+        onRedirectToHome={onRedirectToHome}
+        onSubmit={onSubmit}
+        step={step}
+        t={t}
+      />
+    )
+  }
 
-                  <Col
-                    align="center"
-                    className={classNames(style.stretchVertically, style.noPaddingBottom)}
-                    desk={6}
-                    palm={6}
-                    tablet={6}
-                    tv={6}
-                  >
-                    <Logo className={style.logo} width={140} />
-
-                    <div className={classNames(style.growContent, style.centerContent)}>
-                      <SelfRegisterForm
-                        onRedirectToHome={onRedirectToHome}
-                        onSubmit={onSubmit}
-                        step={step}
-                        t={t}
-                      />
-                    </div>
-                  </Col>
-
-                  <Col tv={3} desk={3} tablet={3} palm={3} />
-                </Row>
-              </Grid>
-            </CardContent>
-          </Card>
-        </Col>
-      </Row>
-
-      <Row className={style.rowBullet}>
-        <Col tv={12} desk={12} tablet={12} palm={12} className={style.stepsMiddle}>
-          <SelfRegisterBulletSteps
-            step={step}
-          />
-        </Col>
-      </Row>
-    </Grid>
-  </div>
-)
+  return (
+    <SelfRegisterWaiting
+      onRedirectToHome={onRedirectToHome}
+      step={step}
+      t={t}
+    />
+  )
+}
 
 SelfRegister.propTypes = {
   onPreviousButton: PropTypes.func.isRequired,
