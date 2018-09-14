c40184813d4e4f242b8e473709235aad4bbe2a4d
deivis <deiviswingert@hotmail.com>
2018-09-13 22:28:55 -0300

    cockpit: refactor company register

--- packages/cockpit/src/index.js
+++ packages/cockpit/src/index.js
@@ -8,6 +8,11 @@ import company from './company'
 import invites from './invites'
 import recipient from './recipient'
 import transactions from './transactions'
+import register from './company/register'
+
+const createCompany = register
+
+export { createCompany }
 
 const cockpit = applySpec({
   balance,
