7930ad2db519b0fee5bbc20a74467cab5e0d3105
deivis <deiviswingert@hotmail.com>
2018-09-13 22:29:21 -0300

    self-register: remove unused createCompany.js

--- packages/pilot/src/pages/SelfRegister/createCompany.js
+++
@@ -1,8 +0,0 @@
-import cockpitSelfRegister from 'cockpit/src/company/selfRegister'
-
-const createCompany = async () => {
-  const result = await cockpitSelfRegister.create()
-  return result
-}
-
-export default createCompany
