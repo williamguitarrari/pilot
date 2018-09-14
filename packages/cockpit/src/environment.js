df619eb50f5682a587359149d840cd0dcf2c91e6
deivis <deiviswingert@hotmail.com>
2018-09-13 22:28:22 -0300

    cockpit: add api url to environment

--- packages/cockpit/src/environment.js
+++ packages/cockpit/src/environment.js
@@ -2,4 +2,8 @@ const { REACT_APP_API_ENVIRONMENT } = process.env
 
 const env = REACT_APP_API_ENVIRONMENT === 'live' ? 'live' : 'test'
 
+const apiUrl = 'https://api.pagar.me/1/'
+
+export { apiUrl }
+
 export default env
