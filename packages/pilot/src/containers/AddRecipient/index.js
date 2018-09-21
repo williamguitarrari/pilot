92cc644b71836bfda6199ad0aeb4ec44c14497bc
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2018-09-21 13:07:59 -0300

    recipients: fix page loader under Firefox
    The <Loader> component will still take 100% page space,
    even with it's "visible" property is set to false. This only happens
    in the dashboard page (not in Storybook), and only in Firefox.
    
    This commit adds a workaround for this problem.

--- packages/pilot/src/containers/AddRecipient/index.js
+++ packages/pilot/src/containers/AddRecipient/index.js
@@ -293,7 +293,7 @@ class AddRecipients extends Component {
 
     return (
       <Fragment>
-        <Loader visible={isLoading} />
+        { isLoading && <Loader visible /> }
         <Card>
           <Steps
             status={stepsStatus}
