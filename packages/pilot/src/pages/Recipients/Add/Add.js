9e8f03f71a3e7e79388fab67912e9080128367c1
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2018-09-27 12:05:27 -0300

    recipients: add configuration options
    - Do not display 'automatic_1025' and 'automatic_dx' anticipation
      models if the 'canConfigureAnticipation' option is set to false
    - Add a minimum delay from the 'minimumAnticipationDays' option
      for antecipation days

--- packages/pilot/src/pages/Recipients/Add/Add.js
+++ packages/pilot/src/pages/Recipients/Add/Add.js
@@ -11,15 +11,23 @@ const mapStateToProps = (state) => {
   const { account } = state
   const { client, company } = account
   const { anticipation_config: anticipationConfig } = company || {}
-  const {
-    config_anticipation_params: canConfigureAnticipation,
-    minimum_delay: minimumAnticipationDays,
+
+  let {
+    config_anticipation_params: anticipation,
+    minimum_delay: delay,
   } = anticipationConfig || {}
+
+  if (anticipation === undefined) anticipation = true
+  if (delay === undefined) delay = 15
+
+  const options = {
+    canConfigureAnticipation: anticipation,
+    minimumAnticipationDays: delay,
+  }
 
   return {
     client,
-    canConfigureAnticipation,
-    minimumAnticipationDays,
+    options,
   }
 }
 
@@ -56,19 +64,12 @@ class AddRecipientPage extends Component {
   }
 
   render () {
-    // TODO: Não mostrar 10/25 e d+x se a company não puder configurar d+x
-    // (config_anticipation_params)
-    // TODO: Prazo de recebimento d+x tem um valor minimo (minimum_delay)
-    /* eslint-disable no-console */
-    console.log('canConfigureAnticipation', this.props.canConfigureAnticipation)
-    console.log('minimumAnticipationDays', this.props.minimumAnticipationDays)
-    /* eslint-enable no-console */
-
     return (
       <AddRecipient
         fetchAccounts={this.fetchAccounts}
         onExit={this.onExit}
         onViewDetails={this.onViewDetails}
+        options={this.props.options}
         submitRecipient={this.submitRecipient}
         t={this.props.t}
       />
@@ -83,17 +84,14 @@ AddRecipientPage.propTypes = {
       bankAccount: PropTypes.func.isRequired,
     }).isRequired,
   }).isRequired,
-  canConfigureAnticipation: PropTypes.bool,
-  minimumAnticipationDays: PropTypes.number,
+  options: PropTypes.shape({
+    canConfigureAnticipation: PropTypes.bool,
+    minimumAnticipationDays: PropTypes.number,
+  }).isRequired,
   t: PropTypes.func.isRequired,
   history: PropTypes.shape({
     replace: PropTypes.func,
   }).isRequired,
-}
-
-AddRecipientPage.defaultProps = {
-  canConfigureAnticipation: true,
-  minimumAnticipationDays: 15,
 }
 
 export default enhanced(AddRecipientPage)
