7479587372b44f8ac2e35a4cf9df77b58506eda7
rsmelo <rodrigosm87@gmail.com>
2018-10-04 17:52:13 -0300

    validation: add email
    Co-authored-by: moog <mateus.moog@gmail.com>

---
+++ packages/pilot/src/validation/email.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-useless-escape */
+const isEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+/* eslint-enable no-useless-escape */
+
+export default message => value => !isEmail.test(value) && message
