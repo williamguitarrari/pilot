23f6ce4edbf90e11b5f64fd86c3cf898fdb7d809
Wallace Oliveira <wallace.oliveira@pagar.me>
2018-10-04 16:06:42 -0300

    validations: add cpf and cnpj validations

---
+++ packages/pilot/src/validation/cnpj.js
@@ -0,0 +1,4 @@
+import { CNPJ } from 'cpf_cnpj'
+
+export default message => value =>
+  !CNPJ.isValid(value) && message
---
+++ packages/pilot/src/validation/cpf.js
@@ -0,0 +1,4 @@
+import { CPF } from 'cpf_cnpj'
+
+export default message => value =>
+  !CPF.isValid(value) && message
