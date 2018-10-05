1ec61e39c5363e4c97962f2dc6320d5cd04e8121
gmbarroso <barroso.guilherme@gmail.com>
2018-10-05 17:15:50 -0300

    recipients-details-cockpit: add page cockpit

--- packages/cockpit/src/recipient/add/formatToRecipient.test.js
+++ packages/cockpit/src/recipient/add/formatToRecipient.test.js
@@ -1,10 +1,19 @@
-import formatToRecipient from './formatToRecipient'
+import { formatToRecipient, formatToBankAccount } from './recipientBuilders'
 import beforeFormat from './mocks/beforeFormat.json'
 import afterFormat from './mocks/afterFormat.json'
+import beforeBankFormat from './mocks/beforeBankFormat.json'
+import afterBankFormat from './mocks/afterBankFormat.json'
+import beforeBankFormatID from './mocks/beforeBankFormatID.json'
+import afterBankFormatID from './mocks/afterBankFormatID.json'
 
-// TODO: add more tests
 describe('Format To Recipient', () => {
   it('formats data correctly', () => {
     expect(formatToRecipient(beforeFormat)).toEqual(afterFormat)
+  })
+  it('format bank accounts', () => {
+    expect(formatToBankAccount(beforeBankFormat)).toEqual(afterBankFormat)
+  })
+  it('format bank accounts with ID', () => {
+    expect(formatToBankAccount(beforeBankFormatID)).toEqual(afterBankFormatID)
   })
 })
