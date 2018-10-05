e04791b8bfa592d935372d80898f4914e37a683c
gmbarroso <barroso.guilherme@gmail.com>
2018-10-05 17:15:50 -0300

    recipients-details-cockpit: add page cockpit

--- packages/cockpit/src/recipient/account/index.js
+++ packages/cockpit/src/recipient/account/index.js
@@ -3,7 +3,7 @@ import formatBankAccount from './formatBankAccount'
 const bankAccount = client => (documentData) => {
   const document = documentData[documentData.documentType]
   const documentNumber = document.replace(/\D/g, '')
-  const query = { document_number: documentNumber }
+  const query = { document_number: documentNumber, count: 50 }
 
   return client.bankAccounts.all(query)
     .then(formatBankAccount)
