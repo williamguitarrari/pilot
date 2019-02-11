ea4873d94d564fe9882a2eee6cfe35b3e07d85ac
Deivis <deiviswingert@hotmail.com>
2019-02-11 20:02:23 -0200

    chore: refactor DevTools

--- packages/pilot/src/App.js
+++ packages/pilot/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { ThemeProvider } from 'former-kit'
 import theme from 'former-kit-skin-pagarme'
 import { HashRouter } from 'react-router-dom'
@@ -7,18 +7,11 @@ import { Provider as StateProvider } from 'react-redux'
 import store from './configureStore'
 import Root from './pages/Root'
 
-const DevTools = process.env.NODE_ENV !== 'production'
-  ? require('./DevTools').default /* eslint-disable-line global-require */
-  : Fragment
-
 const App = () => (
   <ThemeProvider theme={theme}>
     <StateProvider store={store}>
       <HashRouter>
-        <Fragment>
-          <Root />
-          <DevTools />
-        </Fragment>
+        <Root />
       </HashRouter>
     </StateProvider>
   </ThemeProvider>
