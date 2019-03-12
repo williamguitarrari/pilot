17d7e5e861147acd831e49b4dc7f120866a442b8
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-03-12 18:09:46 -0300

    recipients: add gtm virtual page views

--- packages/pilot/src/vendor/googleTagManager.js
+++ packages/pilot/src/vendor/googleTagManager.js
@@ -72,11 +72,20 @@ export const inactiveCompanyLogin = () => {
 /**
  * Trigger activeCompanyLogin event
  */
-
 export const activeCompanyLogin = () => {
   if (hasProperty(window.dataLayer)) {
     window.dataLayer.push({
       event: 'activeCompanyLogin',
+    })
+  }
+}
+
+export const virtualPageView = ({ path, title }) => {
+  if (hasProperty(window.dataLayer)) {
+    window.dataLayer.push({
+      event: 'virtualPageView',
+      pagePath: path,
+      pageTitle: title,
     })
   }
 }
