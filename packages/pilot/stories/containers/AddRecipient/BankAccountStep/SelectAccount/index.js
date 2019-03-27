893996edd49c4b1f2efb5adb7df20bad89589d0a
gmbarroso <guilherme.barroso@pagar.me>
2019-03-27 18:52:06 -0300

    spacing: update stories

--- packages/pilot/stories/containers/AddRecipient/BankAccountStep/SelectAccount/index.js
+++ packages/pilot/stories/containers/AddRecipient/BankAccountStep/SelectAccount/index.js
@@ -28,11 +28,56 @@ const exampleAccounts = [
   },
 ]
 
+const exampleOfSharedData = {
+  identification: {
+    cnpj: '11.111.111/1111-11',
+    cnpjEmail: 'pbftech@email.com',
+    cnpjInformation: true,
+    cnpjName: 'PBF Tecnologia',
+    cnpjPhone: '21 2222-2222',
+    cnpjUrl: 'http://www.pbftec.com.br',
+    cpf: '111-111-111-11',
+    cpfEmail: 'barroso@barroso.com',
+    cpfInformation: false,
+    cpfName: 'Guilherme Melo Barroso',
+    cpfPhone: '21 99999-9999',
+    cpfUrl: '',
+    documentType: 'cnpj',
+    partner0: {
+      cpf: '222.222.222-22',
+      email: 'email@mail.com',
+      name: 'Paulo Barroso',
+    },
+    partner1: {
+      cpf: '111.111.111-11',
+      email: 'email@mail.com',
+      name: 'Guilherme Barroso',
+    },
+    partner2: {
+      cpf: '',
+      email: '',
+      name: '',
+    },
+    partner3: {
+      cpf: '',
+      email: '',
+      name: '',
+    },
+    partner4: {
+      cpf: '',
+      email: '',
+      name: '',
+    },
+    partnerNumber: '2',
+  },
+}
+
 const SelectAccountExample = () => (
   <Section>
     <Card>
       <SelectAccount
         accounts={exampleAccounts}
+        sharedData={exampleOfSharedData}
         onContinue={action('Continue')}
         onBack={action('Back')}
         onCancel={action('Cancel')}
