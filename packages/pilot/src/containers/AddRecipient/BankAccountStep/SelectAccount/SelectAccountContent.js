8595203512d41933d833bd80cc5258b0da796295
gmbarroso <barroso.guilherme@gmail.com>
2019-05-15 18:25:15 -0300

    config: update Dropdown selected option

--- packages/pilot/src/containers/AddRecipient/BankAccountStep/SelectAccount/SelectAccountContent.js
+++ packages/pilot/src/containers/AddRecipient/BankAccountStep/SelectAccount/SelectAccountContent.js
@@ -7,30 +7,37 @@ import {
 } from 'former-kit'
 
 const SelectAccount = ({
+  onChange,
   options,
   t,
+  value,
 }) => (
   <Row>
     <Col tv={2} desk={4} tablet={5} palm={8}>
       <FormDropdown
         label={t('pages.add_recipient.select_bank_account')}
         name="id"
+        onChange={onChange}
         options={options}
+        value={value}
       />
     </Col>
   </Row>
 )
 
 SelectAccount.propTypes = {
+  onChange: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
   })),
   t: PropTypes.func.isRequired,
+  value: PropTypes.string,
 }
 
 SelectAccount.defaultProps = {
   options: [],
+  value: '',
 }
 
 export default SelectAccount
