eafaf2e1d531944efde08a5dffa1b7165687f6a6
Deivis <deiviswingert@hotmail.com>
2019-05-27 15:13:51 -0300

    cockpit: add balance total payables mocks

--- packages/cockpit/src/balance/total/operations.test.js
+++ packages/cockpit/src/balance/total/operations.test.js
@@ -1,6 +1,6 @@
 import buildBalanceTotal from './operations'
-import fromRequest from '../mocks/totalFromRequestsMock.json'
-import expectedResult from '../mocks/totalExpectedResult.json'
+import fromRequest from '../mocks/total/operations/received.json'
+import expectedResult from '../mocks/total/operations/expected.json'
 
 describe('balance total details', () => {
   it('should build the correct total balance', () => {
