202495190da2e533194821b97b3563fee9d5e96b
Vagner S <vagnervst17@gmail.com>
2019-05-31 19:15:52 -0300

    transaction-details: add boleto issue button

--- packages/pilot/src/components/PaymentBoleto/index.js
+++ packages/pilot/src/components/PaymentBoleto/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 import {
   Card,
   CardContent,
-
   Button,
 } from 'former-kit'
 
 import IconCopy from 'emblematic-icons/svg/Copy24.svg'
 import IconBarCode from 'emblematic-icons/svg/BarCode24.svg'
+import BoletoBarcode from './boletoBarcode.svg'
 
 import CopyButton from '../CopyButton'
 
@@ -37,22 +37,32 @@ const PaymentBoleto = ({
           {showBoletoLabel}
         </Button>
       </div>
-      <div className={style.cardBarCode}>
-        <IconBarCode width={25} height={25} />
-        <strong>{barcode}</strong>
-      </div>
-      <div className={style.cardDueDate}>
-        <p>{dueDateLabel} {dueDate}</p>
-        <CopyButton
-          feedbackText={copyBarcodeFeedback}
-          feedbackTimeout={3000}
-          fill="clean"
-          icon={<IconCopy width="12px" height="12px" />}
-          onClick={() => onCopy(barcode)}
-          size="tiny"
-          title={copyBarcodeLabel}
-        />
-      </div>
+      { barcode
+        ? (
+          <div className={style.cardBarCode}>
+            <IconBarCode width={25} height={25} />
+            <strong>{barcode}</strong>
+          </div>
+        ) : (
+          <div className={style.cardBoletoIssue}>
+            <BoletoBarcode />
+          </div>
+        )
+      }
+      { barcode &&
+        <div className={style.cardDueDate}>
+          <p>{dueDateLabel} {dueDate}</p>
+          <CopyButton
+            feedbackText={copyBarcodeFeedback}
+            feedbackTimeout={3000}
+            fill="clean"
+            icon={<IconCopy width="12px" height="12px" />}
+            onClick={() => onCopy(barcode)}
+            size="tiny"
+            title={copyBarcodeLabel}
+          />
+        </div>
+      }
     </CardContent>
   </Card>
 )
