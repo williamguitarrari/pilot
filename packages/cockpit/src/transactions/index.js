7814a9dff82df5933aa63eeba3373bfb4bacca2e
Vagner S <vagnervst17@gmail.com>
2019-07-15 12:01:41 -0300

    cockpit: add transactions metrics interface
    Co-Authored-by: Miguel <oieusouamiguel@gmail.com>

--- packages/cockpit/src/transactions/index.js
+++ packages/cockpit/src/transactions/index.js
@@ -3,10 +3,12 @@ import details from './details'
 import reprocess from './reprocess'
 import search from './search'
 import exportData from './export'
+import metrics from './metrics'
 
 export default {
   countPendingReviews,
   details,
+  metrics,
   reprocess,
   search,
   exportData,
