46cb7f7934c7b9004f2928279d131e75e26cbf8e
Vagner S <vagnervst17@gmail.com>
2019-07-23 15:49:25 -0300

    components: add MetricChart

---
+++ packages/pilot/src/components/MetricChart/charts/BarChart.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import { identity } from 'ramda'
+
+import sizePropValidation from '../sizePropValidation'
+
+const cursorProps = {
+  fillOpacity: 0.15,
+}
+
+const MetricBarChart = ({
+  data,
+  labelFormatter,
+  styles: {
+    barSize,
+    colors,
+    cursor,
+    gridLines,
+    height,
+    width,
+  },
+}) => (
+  <ResponsiveContainer
+    height={height}
+    width={width}
+  >
+    <BarChart data={data}>
+      <CartesianGrid
+        strokeDasharray={gridLines || '3 3'}
+        vertical={false}
+      />
+      <XAxis
+        axisLine={false}
+        dataKey="label"
+        tickLine={false}
+        tickMargin={10}
+      />
+      <YAxis
+        axisLine={false}
+        tickLine={false}
+        tickMargin={10}
+      />
+      <Tooltip
+        cursor={cursor || cursorProps}
+        formatter={value => [value]}
+        labelFormatter={labelFormatter}
+      />
+      <Bar
+        barSize={barSize || 20}
+        dataKey="value"
+        fill={colors.fill}
+      />
+    </BarChart>
+  </ResponsiveContainer>
+)
+
+MetricBarChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      color: PropTypes.string,
+      label: PropTypes.string.isRequired,
+      legendRenderer: PropTypes.func,
+      value: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  labelFormatter: PropTypes.func,
+  styles: PropTypes.shape({
+    barSize: PropTypes.number,
+    colors: PropTypes.shape({
+      fill: PropTypes.string.isRequired,
+    }).isRequired,
+    cursor: PropTypes.object,
+    gridLines: PropTypes.string,
+    height: sizePropValidation,
+    width: sizePropValidation,
+  }).isRequired,
+}
+
+MetricBarChart.defaultProps = {
+  labelFormatter: identity,
+}
+
+export default MetricBarChart
