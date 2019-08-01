59a140bd7b338da5cf5f0bfddabc703ca4632b01
Deivis <deiviswingert@hotmail.com>
2019-08-01 18:04:15 -0300

    chore: refactor MetricList to use MetricCard

--- packages/pilot/src/components/MetricList/index.js
+++ packages/pilot/src/components/MetricList/index.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
-  Card,
   CardTitle,
   CardContent,
 } from 'former-kit'
-
+import MetricCard from '../MetricCard'
 import List from './List'
 
 import styles from './style.css'
 
 const MetricList = ({
   items,
+  loading,
   title,
 }) => (
-  <Card>
+  <MetricCard loading={loading}>
     <CardTitle title={title} />
     <CardContent className={styles.list}>
       <List items={items} />
     </CardContent>
-  </Card>
+  </MetricCard>
 )
 
 MetricList.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape(List.propTypes.items)
   ).isRequired,
+  loading: PropTypes.bool,
   title: PropTypes.string.isRequired,
+}
+
+MetricList.defaultProps = {
+  loading: false,
 }
 
 export default MetricList
