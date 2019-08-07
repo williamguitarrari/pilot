b3b6d350e2d1885e6f4297214b53d30e5b256e04
Vagner S <vagnervst17@gmail.com>
2019-08-07 11:52:21 -0300

    chore: refactor MetricCard
    Co-authored-by: Deivis <deiviswingert@hotmail.com>

--- packages/pilot/src/components/MetricCard/index.js
+++ packages/pilot/src/components/MetricCard/index.js
@@ -1,33 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  Card,
-  CardContent,
-  CardTitle,
-} from 'former-kit'
+import { Card } from 'former-kit'
+import withSpinner from '../withSpinner'
 
 import styles from './style.css'
 
-const MetricCard = ({
-  icon,
-  title,
-  value,
-}) => (
-  <Card>
-    <CardTitle title={title} />
-    <CardContent>
-      <div className={styles.info}>
-        <h1 className={styles.value}>{value}</h1>
-        <span className={styles.icon}>{icon}</span>
-      </div>
-    </CardContent>
-  </Card>
+const enhance = withSpinner(styles.overlay)
+
+const MetricCard = ({ children }) => (
+  <div className={styles.metricCard}>
+    <Card>
+      {children}
+    </Card>
+  </div>
 )
 
 MetricCard.propTypes = {
-  icon: PropTypes.node.isRequired,
-  title: PropTypes.string.isRequired,
-  value: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 }
 
-export default MetricCard
+export default enhance(MetricCard)
