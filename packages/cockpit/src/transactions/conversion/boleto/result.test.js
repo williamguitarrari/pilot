348b5c1dba2de5b27b606ea8f0589a58b2b9daf9
miguelssrs <oieusouamiguel@gmail.com>
2019-08-14 20:22:31 -0300

    cockpit: add boleto conversion request

--- packages/cockpit/src/transactions/conversion/boleto/result.test.js
+++ packages/cockpit/src/transactions/conversion/boleto/result.test.js
@@ -3,7 +3,7 @@ import buildResult from './result'
 import fromRequest from './mocks/fromRequest.json'
 import expectedResult from './mocks/expectedResult.json'
 
-describe('Transactions Card Convertion', () => {
+describe('Transactions Boleto Conversion', () => {
   it('should build result as expected', () => {
     const result = buildResult(fromRequest)
     expect(result).toEqual(expectedResult)
