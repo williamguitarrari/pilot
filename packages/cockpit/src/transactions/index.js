2051817cea072535f93ac4aa15bcbe2a035fb363
miguelssrs <oieusouamiguel@gmail.com>
2019-08-14 20:22:50 -0300

    cockpit: add conversion request

--- packages/cockpit/src/transactions/index.js
+++ packages/cockpit/src/transactions/index.js
@@ -1,3 +1,4 @@
+import conversion from './conversion'
 import countPendingReviews from './countPendingReviews'
 import details from './details'
 import reprocess from './reprocess'
@@ -6,6 +7,7 @@ import exportData from './export'
 import metrics from './metrics'
 
 export default {
+  conversion,
   countPendingReviews,
   details,
   metrics,
