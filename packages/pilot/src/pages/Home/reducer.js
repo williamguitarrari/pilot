c5219b539569c9a1325995c4f89aa50f34031ca7
Deivis <deiviswingert@hotmail.com>
2019-08-27 17:15:35 -0300

    home: add conversion actions

--- packages/pilot/src/pages/Home/reducer.js
+++ packages/pilot/src/pages/Home/reducer.js
@@ -1,11 +1,17 @@
+import { assocPath, mergeDeepRight } from 'ramda'
 import {
+  CONVERSION_RECEIVE,
+  CONVERSION_REQUEST,
+  CONVERSION_REQUEST_FAIL,
   METRICS_RECEIVE,
   METRICS_REQUEST,
   METRICS_REQUEST_FAIL,
 } from './actions'
 
 const initialState = {
+  conversion: null,
   loading: {
+    conversion: true,
     metrics: true,
   },
   metrics: null,
@@ -14,11 +20,7 @@ const initialState = {
 export default function anticipationReducer (state = initialState, action) {
   switch (action.type) {
     case METRICS_REQUEST: {
-      return {
-        loading: {
-          metrics: true,
-        },
-      }
+      return assocPath(['loading', 'metrics'], true, state)
     }
 
     case METRICS_RECEIVE: {
@@ -26,21 +28,37 @@ export default function anticipationReducer (state = initialState, action) {
         payload,
       } = action
 
-      return {
+      return mergeDeepRight(state, {
         loading: {
           metrics: false,
         },
         metrics: payload,
-      }
+      })
     }
 
     case METRICS_REQUEST_FAIL: {
-      return {
-        ...state,
+      return assocPath(['loading', 'metrics'], false, state)
+    }
+
+    case CONVERSION_REQUEST: {
+      return assocPath(['loading', 'conversion'], true, state)
+    }
+
+    case CONVERSION_RECEIVE: {
+      const {
+        payload,
+      } = action
+
+      return mergeDeepRight(state, {
+        conversion: payload,
         loading: {
-          metrics: false,
+          conversion: false,
         },
-      }
+      })
+    }
+
+    case CONVERSION_REQUEST_FAIL: {
+      return assocPath(['loading', 'conversion'], false, state)
     }
 
     default: {
