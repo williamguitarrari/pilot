620da5a3602da0fb6871d6dc1c2d57d8912e1e7b
Deivis <deiviswingert@hotmail.com>
2019-08-28 14:10:49 -0300

    cockpit: refactor conversion value

--- packages/cockpit/src/transactions/conversion/result.js
+++ packages/cockpit/src/transactions/conversion/result.js
@@ -1,6 +1,17 @@
+import {
+  assoc,
+  propEq,
+  when,
+} from 'ramda'
+
+const formatConversion = when(
+  propEq('total', 0),
+  assoc('conversion', 0)
+)
+
 const buildResult = ([cardResult, boletoResult]) => ({
-  card: cardResult,
-  boleto: boletoResult,
+  card: formatConversion(cardResult),
+  boleto: formatConversion(boletoResult),
 })
 
 export default buildResult
