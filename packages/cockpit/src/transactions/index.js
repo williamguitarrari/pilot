db254c001bf11d8c6e807d54675854e3b4153289
miguelssrs <oieusouamiguel@gmail.com>
2019-10-03 15:55:15 -0300

    cockpit: refactor reprocess method

--- packages/cockpit/src/transactions/index.js
+++ packages/cockpit/src/transactions/index.js
@@ -1,7 +1,7 @@
 import conversion from './conversion'
 import countPendingReviews from './countPendingReviews'
 import details from './details'
-import reprocess from './reprocess'
+import reprocessWithAntifraud from './reprocess'
 import search from './search'
 import exportData from './export'
 import metrics from './metrics'
@@ -11,7 +11,7 @@ export default {
   countPendingReviews,
   details,
   metrics,
-  reprocess,
+  reprocessWithAntifraud,
   search,
   exportData,
 }
