4a7ae05c0ed0a937ebec6b67cf23d94c21d629f9
0liveira <trueoliveir4@gmail.com>
2019-10-17 17:21:07 -0300

    cockpit: fix reprocess transactions payload

--- packages/cockpit/src/transactions/reprocess/cleanTransaction.test.js
+++ packages/cockpit/src/transactions/reprocess/cleanTransaction.test.js
@@ -1,11 +1,19 @@
 import {
   assoc,
+  assocPath,
   dissoc,
+  dissocPath,
   pipe,
 } from 'ramda'
 import cleanTransaction from './cleanTransaction'
 import fromRequest from './mocks/fromRequest.json'
 import toRequest from './mocks/toRequest.json'
+
+const buildCustomerObject = ({ address, phone, customer }) => ({
+  address,
+  phone,
+  ...customer,
+})
 
 describe('Reprocess', () => {
   it('should work', () => {
@@ -20,5 +28,22 @@ describe('Reprocess', () => {
     )(toRequest)
 
     expect(cleanTransaction(withoutCustomer)).toEqual(toRequestWithourCustomer)
+  })
+
+  it('should work when documents is empty', () => {
+    const withoutCustomer = assocPath(['customer', 'documents'], [], fromRequest)
+    const customerObject = buildCustomerObject(fromRequest)
+
+    const toRequestExpected = pipe(
+      assoc('customer', customerObject),
+      assocPath(['customer', 'documents'], []),
+      dissocPath(['customer', 'born_at']),
+      dissocPath(['customer', 'document_number']),
+      dissocPath(['customer', 'gender']),
+      dissocPath(['customer', 'phone', 'id']),
+      dissocPath(['customer', 'phone', 'object']),
+    )(toRequest)
+
+    expect(cleanTransaction(withoutCustomer)).toEqual(toRequestExpected)
   })
 })
