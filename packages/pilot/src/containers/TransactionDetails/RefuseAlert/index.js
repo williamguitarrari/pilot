a132dee6cefbcfe6cd5c56f453d5a02756216117
marisa <ma-ah02@live.com>
2019-12-18 14:11:45 -0300

    TransactionDetails: add RefuseAlert component

---
+++ packages/pilot/src/containers/TransactionDetails/RefuseAlert/index.js
@@ -0,0 +1,73 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+import { isNil } from 'ramda'
+import {
+  Alert,
+  Col,
+  Row,
+} from 'former-kit'
+import IconInfo from 'emblematic-icons/svg/Info32.svg'
+import style from './style.css'
+
+import getRefuseLabel from '../../../models/refuseLabels'
+
+const RefuseAlert = ({
+  acquirer,
+  status,
+  statusReason,
+  t,
+}) => {
+  const refuseCode = acquirer.response_code
+  const emptyRefuseCode = '0000'
+
+  const refuseLabels = statusReason === 'acquirer'
+    ? getRefuseLabel(refuseCode)
+    : getRefuseLabel(statusReason)
+
+  return (
+    <Fragment>
+      {(status === 'refused')
+        && (
+          <Row className={style.alertCustom}>
+            <Col
+              desk={12}
+              palm={12}
+              tablet={12}
+              tv={12}
+            >
+              <Alert
+                icon={<IconInfo height={16} width={16} />}
+                type="error"
+              >
+                <span className={style.refuseAlertCustom}>
+                  <b>{t('pages.transaction.refuse_reason')}</b>
+                  {refuseCode !== emptyRefuseCode
+                    && !isNil(refuseCode)
+                    && (
+                      <span className={style.refuseCode}>
+                        {refuseCode}
+                      </span>
+                    )
+                  }
+                  {t(refuseLabels.reason)} {t(refuseLabels.action)}
+                </span>
+              </Alert>
+            </Col>
+          </Row>
+        )
+      }
+    </Fragment>
+  )
+}
+
+RefuseAlert.propTypes = {
+  acquirer: PropTypes.shape({
+    response_code: PropTypes.string,
+  }).isRequired,
+  status: PropTypes.string.isRequired,
+  statusReason: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired,
+}
+
+export default RefuseAlert
