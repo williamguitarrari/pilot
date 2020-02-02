bb8510db1e54128d27ee2dd66a979a0c23a56e3c
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2020-02-02 18:03:59 -0300

    empty-state: add AccessKeys

--- packages/pilot/src/containers/EmptyState/index.js
+++ packages/pilot/src/containers/EmptyState/index.js
@@ -1,5 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import AccessKeys from './AccessKeys'
 
-const EmptyState = () => <div />
+const EmptyState = ({
+  apiKey,
+  encryptionKey,
+  environment,
+  t,
+}) => (
+  <div>
+    <AccessKeys
+      apiKey={apiKey}
+      encryptionKey={encryptionKey}
+      environment={environment}
+      t={t}
+    />
+  </div>
+)
+
+EmptyState.propTypes = {
+  apiKey: PropTypes.string,
+  encryptionKey: PropTypes.string,
+  environment: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired,
+}
+
+EmptyState.defaultProps = {
+  apiKey: '',
+  encryptionKey: '',
+}
 
 export default EmptyState
