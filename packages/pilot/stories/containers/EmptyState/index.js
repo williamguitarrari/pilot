f91f34cbb8498cc330632c7981509396d3bfacfb
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2020-02-02 18:03:59 -0300

    empty-state: add AccessKeys

--- packages/pilot/stories/containers/EmptyState/index.js
+++ packages/pilot/stories/containers/EmptyState/index.js
@@ -3,6 +3,15 @@ import React from 'react'
 import EmptyState from '../../../src/containers/EmptyState'
 import Section from '../../Section'
 
-const EmptyStateExample = () => <Section><EmptyState /></Section>
+const EmptyStateExample = () => (
+  <Section>
+    <EmptyState
+      apiKey="api-key"
+      encryptionKey="encryption-key"
+      environment="live"
+      t={t => t}
+    />
+  </Section>
+)
 
 export default EmptyStateExample
