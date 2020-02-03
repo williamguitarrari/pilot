d549ef189f1bb7e6fd94d2b180b689f3d09d6180
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2020-02-02 23:09:42 -0300

    pages: add empty-state

---
+++ packages/pilot/src/pages/EmptyState/index.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import {
+  always,
+  applySpec,
+  compose,
+  equals,
+  find,
+  head,
+  not,
+  path,
+  pathOr,
+  pluck,
+  pipe,
+  prop,
+  propEq,
+  split,
+  when,
+} from 'ramda'
+import { translate } from 'react-i18next'
+import EmptyStateContainer from '../../containers/EmptyState'
+import { withError } from '../ErrorBoundary'
+import environment from '../../environment'
+
+const getUserName = pipe(prop('name'), split(' '), head)
+
+const getAccessKeys = applySpec({
+  apiKey: path(['api_key', environment]),
+  encryptionKey: path(['encryption_key', environment]),
+})
+
+const getAntifraudCost = pipe(
+  pathOr([], ['gateway', environment, 'antifraud_cost']),
+  find(propEq('name', 'pagarme')),
+  prop('cost')
+)
+
+const notDefaultInstallments = pipe(
+  pluck('installment'),
+  equals([1, 2, 7]),
+  not
+)
+
+const getInstallmentsFee = pipe(
+  pathOr([], ['psp', environment, 'mdrs']),
+  find(propEq('payment_method', 'credit_card')),
+  pathOr([], ['installments']),
+  when(notDefaultInstallments, always([]))
+)
+
+const getFees = pipe(
+  prop('pricing'),
+  applySpec({
+    anticipation: path(['psp', environment, 'anticipation']),
+    antifraud: getAntifraudCost,
+    boleto: path(['gateway', environment, 'transaction_cost', 'boleto']),
+    gateway: path(['gateway', environment, 'transaction_cost', 'credit_card']),
+    installments: getInstallmentsFee,
+    transfer: path(['transfers', 'ted']),
+  })
+)
+
+const mapStateToProps = ({
+  account: {
+    company,
+    user,
+  },
+}) => ({
+  accessKeys: getAccessKeys(company),
+  fees: getFees(company),
+  userName: getUserName(user),
+})
+
+const enhanced = compose(
+  translate(),
+  connect(mapStateToProps),
+  withError
+)
+
+const hideEmptyState = push => () => {
+  localStorage.setItem('hide_empty-state', true)
+  return push('/home')
+}
+
+const EmptyState = ({
+  accessKeys, fees, history, t, userName,
+}) => (
+  <EmptyStateContainer
+    apiKey={accessKeys.apiKey}
+    encryptionKey={accessKeys.encryptionKey}
+    environment={environment}
+    fees={fees}
+    onDisableWelcome={hideEmptyState(history.push)}
+    t={t}
+    userName={userName}
+  />
+)
+
+EmptyState.propTypes = {
+  accessKeys: PropTypes.shape({
+    apiKey: PropTypes.string,
+    encryptionKey: PropTypes.string,
+  }),
+  fees: PropTypes.shape({
+    anticipation: PropTypes.number,
+    antifraud: PropTypes.number,
+    boleto: PropTypes.number,
+    gateway: PropTypes.number,
+    installments: PropTypes.arrayOf(PropTypes.shape({
+      installment: PropTypes.number.isRequired,
+      mdr: PropTypes.number.isRequired,
+    })),
+    transfer: PropTypes.number,
+  }),
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  t: PropTypes.func.isRequired,
+  userName: PropTypes.string,
+}
+
+EmptyState.defaultProps = {
+  accessKeys: {},
+  fees: {},
+  userName: '',
+}
+
+export default enhanced(EmptyState)
