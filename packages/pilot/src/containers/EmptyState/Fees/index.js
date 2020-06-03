42d57985775317131664874edd64a99af8fa62e8
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2020-06-03 12:56:39 -0300

    empty-state: refactor fees details

--- packages/pilot/src/containers/EmptyState/Fees/index.js
+++ packages/pilot/src/containers/EmptyState/Fees/index.js
@@ -1,106 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Flexbox } from 'former-kit'
-import { map } from 'ramda'
-import FeeTitleAndValues from './FeeTitleAndValues'
 import CollapsibleCard from '../CollapsibleCard'
+import FeesDetails from '../../../components/FeesDetails'
 import styles from './styles.css'
-
-const translationPathByInstallment = {
-  1: 'pages.empty_state.fees.one_installment',
-  2: 'pages.empty_state.fees.two_to_six_installments',
-  7: 'pages.empty_state.fees.seven_or_more_installments',
-}
-
-const buildInstallmentsValues = map(({ installment, mdr }) => ({
-  translationPath: translationPathByInstallment[installment],
-  type: 'percent',
-  value: mdr,
-}))
-
-const loadFirstInstallmentMDR = (installments) => {
-  if (installments.length < 1) {
-    return null
-  }
-
-  return installments[0].mdr
-}
-
-const loadCreditCardFees = ({ fees, isMDRzao }) => (
-  isMDRzao
-    ? [
-      {
-        translationPath: 'pages.empty_state.fees.mdrzao_installment',
-        type: 'percent',
-        value: loadFirstInstallmentMDR(fees.installments),
-        valueSuffixPath: 'pages.empty_state.fees.per_transaction',
-      },
-    ]
-    : [
-      ...buildInstallmentsValues(fees.installments),
-      {
-        translationPath: 'pages.empty_state.fees.processing',
-        type: 'currency',
-        value: fees.gateway,
-      },
-      {
-        translationPath: 'pages.empty_state.fees.antifraud',
-        type: 'currency',
-        value: fees.antifraud,
-      },
-    ])
 
 const Fees = ({ fees, isMDRzao, t }) => {
   const subtitle = <span className={styles.subtitle}>{t('pages.empty_state.fees.subtitle')}</span>
-
-  const creditCardFees = loadCreditCardFees({ fees, isMDRzao })
 
   return (
     <CollapsibleCard
       title={t('pages.empty_state.fees.title')}
       subtitle={subtitle}
     >
-      <FeeTitleAndValues
-        t={t}
-        title={t('pages.empty_state.fees.credit_card')}
-        values={creditCardFees}
-      />
-      <Flexbox className={styles.marginRight}>
-        <FeeTitleAndValues
-          t={t}
-          title={t('pages.empty_state.fees.boleto')}
-          values={[
-            {
-              translationPath: 'pages.empty_state.fees.paid',
-              type: 'currency',
-              value: fees.boleto,
-            },
-          ]}
-        />
-        <FeeTitleAndValues
-          t={t}
-          title={t('pages.empty_state.fees.anticipation')}
-          values={[
-            {
-              translationPath: 'pages.empty_state.fees.tax',
-              type: 'percent',
-              value: fees.anticipation,
-              valueSuffixPath: isMDRzao ? 'pages.empty_state.fees.per_installment' : '',
-            },
-          ]}
-        />
-        <FeeTitleAndValues
-          t={t}
-          title={t('pages.empty_state.fees.transfers')}
-          values={[
-            {
-              translationPath: 'pages.empty_state.fees.doc_ted',
-              type: 'currency',
-              value: fees.transfer,
-            },
-          ]}
-        />
-      </Flexbox>
+      <div className={styles.feeArea}>
+        <FeesDetails fees={fees} isMDRzao={isMDRzao} t={t} />
+      </div>
     </CollapsibleCard>
   )
 }
