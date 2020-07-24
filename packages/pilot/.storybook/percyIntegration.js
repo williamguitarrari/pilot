f69e730e4da4f3c13954c59fc4315a6c305293b5
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2020-07-24 17:26:27 -0300

    chore: refactor optimize circleci build

--- packages/pilot/.storybook/percyIntegration.js
+++ packages/pilot/.storybook/percyIntegration.js
@@ -1,5 +1,4 @@
-import { configure, setAddon, getStorybook } from '@storybook/react'
-import createPercyAddon from '@percy-io/percy-storybook'
+import { configure } from '@storybook/react'
 import mockDate from 'mockdate'
 import inPercy from '@percy-io/in-percy'
 
@@ -7,15 +6,9 @@ if (inPercy()) {
   mockDate.set(1506815400000)
 }
 
-const { percyAddon, serializeStories } = createPercyAddon()
-
 const req = require.context('../stories', true, /\.js$/)
 function loadStories () {
   req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
-setAddon(percyAddon)
-
-serializeStories(getStorybook)
-
