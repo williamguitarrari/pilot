acf99b1a53242883a18b909548ad72c91b06474b
Vagner S <vagnervst17@gmail.com>
2020-08-11 19:40:43 -0300

    sidebar-container: refactor as functional

--- packages/pilot/src/containers/Sidebar/index.js
+++ packages/pilot/src/containers/Sidebar/index.js
@@ -1,11 +1,9 @@
-import React, { isValidElement } from 'react'
+import React, { useState, isValidElement } from 'react'
 import PropTypes from 'prop-types'
 import {
-  propOr,
-  __,
-  ifElse,
-  equals,
   always,
+  equals,
+  ifElse,
 } from 'ramda'
 
 import {
@@ -25,12 +23,8 @@ import IconWallet from 'emblematic-icons/svg/Wallet32.svg'
 
 import style from './style.css'
 
-import SidebarSections from '../../components/SidebarSections'
+import Sections from './Sections'
 import SidebarSummary from '../../components/SidebarSummary'
-import formatDecimalCurrency from '../../formatters/decimalCurrency'
-import isPaymentLink from '../../validation/isPaymentLink'
-
-const MINIMUM_API_VALUE = 100
 
 const getMenuIcon = ifElse(
   equals(true),
@@ -38,155 +32,115 @@ const getMenuIcon = ifElse(
   always(IconMenuNotCollapsed)
 )
 
-class SidebarContainer extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      collapsed: false,
-      summaryCollapsed: false,
-    }
+const SidebarContainer = ({
+  balance,
+  companyName,
+  companyType,
+  links,
+  logo: Logo,
+  onLinkClick,
+  onWithdraw,
+  t,
+  transfersPricing,
+}) => {
+  const [collapsed, setCollapsed] = useState(false)
+  const [summaryCollapsed, setSummaryCollapsed] = useState(false)
 
-    this.handleToggleSidebar = this.handleToggleSidebar.bind(this)
-  }
+  const handleToggleSidebar = () => setCollapsed(!collapsed)
 
-  handleToggleSidebar () {
-    const { collapsed } = this.state
-    this.setState({ collapsed: !collapsed })
-  }
+  const handleToggleSummary = () => setSummaryCollapsed(!summaryCollapsed)
 
-  renderSections () {
-    const {
-      // This block of code is commented because of issue #1159 (https://github.com/pagarme/pilot/issues/1159)
-      // It was commented on to remove the anticipation limits call on Balance page
-      // This code will be used again in the future when ATLAS project implements the anticipation flow
-      // More details in issue #1159
-      // anticipationLimit,
-      balance,
-      companyType,
-      onWithdraw,
-      t,
-      transfersPricing,
-    } = this.props
+  const renderSections = () => (
+    <Sections
+      balance={balance}
+      companyType={companyType}
+      onWithdraw={onWithdraw}
+      t={t}
+      transfersPricing={transfersPricing}
+    />
+  )
 
-    const getFrombalance = propOr(null, __, balance)
-    const available = getFrombalance('available')
+  const MenuIcon = getMenuIcon(collapsed)
 
-    const minimumWithdrawalValue = transfersPricing.ted + MINIMUM_API_VALUE
-    const isCompanyNotPaymentLink = !!companyType && !isPaymentLink(companyType)
+  const summarySubtitle = summaryCollapsed
+    ? t('pages.sidebar.show_balance')
+    : t('pages.sidebar.hide_balance')
 
-    return (
-      <SidebarSections
-        sections={[
-          {
-            action: onWithdraw,
-            actionTitle: t('pages.sidebar.withdraw'),
-            disabled: available <= minimumWithdrawalValue,
-            showButton: isCompanyNotPaymentLink,
-            title: t('pages.sidebar.available'),
-            value: <span><small>{t('pages.sidebar.currency_symbol')}</small> {formatDecimalCurrency(available)}</span>,
-          },
-        ]}
-      />
-    )
-  }
+  return (
+    <Sidebar collapsed={collapsed}>
+      <SidebarHeader>
+        {!collapsed && <Logo width={140} />}
 
-  render () {
-    const {
-      collapsed,
-      summaryCollapsed,
-    } = this.state
-    const {
-      companyName,
-      links,
-      logo: Logo,
-      onLinkClick,
-      t,
-    } = this.props
-
-    const MenuIcon = getMenuIcon(collapsed)
-
-    return (
-      <Sidebar collapsed={collapsed}>
-        <SidebarHeader>
-          {!collapsed && <Logo width={140} />}
-
-          <Button
-            onClick={this.handleToggleSidebar}
-            icon={(
-              <MenuIcon
-                width={24}
-                height={24}
-                className={style.sidebarIcon}
-              />
-            )}
-            fill="clean"
-            relevance="low"
-          />
-        </SidebarHeader>
-
-        {!collapsed
-          && (
-            <SidebarSummary
-              collapsed={summaryCollapsed}
-              onClick={() => this.setState({
-                summaryCollapsed: !summaryCollapsed,
-              })}
-              subtitle={
-                summaryCollapsed
-                  ? t('pages.sidebar.show_balance')
-                  : t('pages.sidebar.hide_balance')
-              }
-              title={companyName}
-            >
-              {this.renderSections()}
-            </SidebarSummary>
-          )
-        }
-
-        {collapsed
-          && (
-            <Popover
-              base="dark"
-              content={(
-                <PopoverContent>
-                  {this.renderSections()}
-                </PopoverContent>
-              )}
-              placement="rightStart"
-            >
-              <SidebarLinks>
-                <SidebarLink
-                  collapsed={collapsed}
-                  onClick={() => null}
-                  icon={<IconWallet width={16} height={16} />}
-                  title={t('pages.sidebar.balance')}
-                />
-              </SidebarLinks>
-            </Popover>
-          )
-        }
-
-        <SidebarLinks>
-          {links.map(({
-            active,
-            exact,
-            icon: Icon,
-            path,
-            title,
-          }) => (
-            <SidebarLink
-              key={path}
-              title={t(title)}
-              active={active}
-              icon={<Icon width={16} height={16} />}
-              collapsed={collapsed}
-              onClick={() => onLinkClick({ active, exact, path })}
+        <Button
+          onClick={handleToggleSidebar}
+          icon={(
+            <MenuIcon
+              width={24}
+              height={24}
+              className={style.sidebarIcon}
             />
-          ))}
-        </SidebarLinks>
-      </Sidebar>
-    )
-  }
+          )}
+          fill="clean"
+          relevance="low"
+        />
+      </SidebarHeader>
+
+      {!collapsed
+        && (
+          <SidebarSummary
+            collapsed={summaryCollapsed}
+            onClick={handleToggleSummary}
+            subtitle={summarySubtitle}
+            title={companyName}
+          >
+            {renderSections()}
+          </SidebarSummary>
+        )
+      }
+
+      {collapsed
+        && (
+          <Popover
+            base="dark"
+            content={(
+              <PopoverContent>
+                {renderSections()}
+              </PopoverContent>
+            )}
+            placement="rightStart"
+          >
+            <SidebarLinks>
+              <SidebarLink
+                collapsed={collapsed}
+                onClick={() => null}
+                icon={<IconWallet width={16} height={16} />}
+                title={t('pages.sidebar.balance')}
+              />
+            </SidebarLinks>
+          </Popover>
+        )
+      }
+
+      <SidebarLinks>
+        {links.map(({
+          active,
+          exact,
+          icon: Icon,
+          path,
+          title,
+        }) => (
+          <SidebarLink
+            key={path}
+            title={t(title)}
+            active={active}
+            icon={<Icon width={16} height={16} />}
+            collapsed={collapsed}
+            onClick={() => onLinkClick({ active, exact, path })}
+          />
+        ))}
+      </SidebarLinks>
+    </Sidebar>
+  )
 }
 
 SidebarContainer.propTypes = {
@@ -197,7 +151,6 @@ SidebarContainer.propTypes = {
   // anticipationLimit: PropTypes.number,
   balance: PropTypes.shape({
     available: PropTypes.number,
-    waitingFunds: PropTypes.number,
   }).isRequired,
   companyName: PropTypes.string,
   companyType: PropTypes.string,
